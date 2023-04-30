8c759ab085c263638b17888fabddc25dcf1f1343
Anthony Conrad <parithon@outlook.com>
2023-04-30 16:02:11 -0700

    Updated build.ps1 to be less relative

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-$Path = "../Terraform/terraform.d/plugins/registry.terraform.io/paultyng/unifi"
+$Path = "../terraform/terraform.d/plugins/registry.terraform.io/paultyng/unifi"
 go build -o "$Path/0.34.1/windows_amd64/terraform-provider-unifi_0.34.1.exe"
